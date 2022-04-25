a1b1540a6db6719cccd4118091554329f7d66fc1
atikalg <who.atik@gmail.com>
2022-04-25 20:30:18 +0530

    Added : Ability to create linked list

--- LinkedList.java
+++ LinkedList.java
@@ -1,7 +1,7 @@
 package com.bridgelab.Assignment_14_LinkedList;
 /*
  * @author : Atik
- * Lets create a simple Linked List of 56, 30 and 70
+ * Ability to create Linked List by adding 30 and 56 to 70
  */class Node {
 	    public int data;
 	    public Node next;
@@ -14,24 +14,48 @@ package com.bridgelab.Assignment_14_LinkedList;
 	}
 
 	class LinkedListData {
-		Node head;
-	    Node tail;
-	    //Insert Data in New Node
-	    public void insertFirst(int data) {
-	        Node newNode = new Node(data);
-	    }
-	}
+		 Node head;
+		    Node tail;
+		    //Insert Data in New Node
+		    public void insertFirst(int data) {
+		        Node newNode = new Node(data);
+		        //Adding Data in Node
+		        if(head == null) {
+		            head = newNode;
+		            tail = newNode;
+		        } else {
+		            newNode.next = head;
+		            head = newNode;
+		        }
+		    }
 
+		    //Display Node in Linked List
+		    public void displayLinkedList() {
+		        if(head == null) {
+		            System.out.println("Linked List is Empty.");
+		        } else {
+		            Node temp = head;
+		            while (temp != null) {
+		                System.out.print(temp.data + " -> ");
+		                temp = temp.next;
+		            }
+		        }
+		    }
+		}
 	public class LinkedList {
 	    public static void main(String[] args) {
-	    	 //Welcome Message
-	        System.out.println("Welcome to LinkedList Data Structures Problems.");
+	    	  //Welcome Message
+	        System.out.println("Welcome to LinkedList Data Structures Problem.");
 
 	        LinkedListData linkedList = new LinkedListData();
+
 	        //Insert 70, 50, 36 in Linked List Nodes
 	        linkedList.insertFirst(70);
 	        linkedList.insertFirst(30);
 	        linkedList.insertFirst(56);
+
+	        //Showing Linked List Data
+	        linkedList.displayLinkedList();
 	    }
 }
 	
